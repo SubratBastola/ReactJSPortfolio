f184f2f8f805098d86c442346ab3c4e784a3688c
SubratBastola <68183902+SubratBastola@users.noreply.github.com>
2020-07-13 09:00:05 +0545

    Add files via upload

---
+++ i18n/en.js
@@ -0,0 +1,27 @@
+export default {
+  Resume: {
+    download: 'Download',
+    hireMe: 'Hire me',
+    workExperienceAndEducation: 'Work experience & Education',
+    years: ' years',
+    year: ' year',
+    skills: 'Skills',
+    languages: 'Languages',
+    interests: 'Interests',
+    resume: 'Resume',
+    projectsAndDevelopments: 'Projects & developments',
+    showMore: 'Show more',
+    showLess: 'Show less',
+  },
+  
+  SimpleBottomNavigation: {
+    aboutMe: 'About me',
+    home: 'Home',
+    workExperience: 'Experience',
+    education: 'Education',
+    skills: 'Skills',
+    languages: 'Languages',
+    hobbies: 'Hobbies',
+    projects: 'Projects',
+  },
+};
